4d30a8f8097489eaa581bcf7ec4dbc95206f248c
Daniil Berestenko <berestenkodaniilki212@gmail.com>
2022-05-03 18:42:11 +0300

    Task_2 updated decreased array amount

--- Lection_3/task_2/main.cpp
+++ Lection_3/task_2/main.cpp
@@ -8,29 +8,17 @@ int main()
     srand(time(0));
     int array1[4],array2[4];
     int summ1 = 0,summ2 = 0;
-    cout << "First array: ";
     for (int i = 0; i < 4; i++){
         array1[i] = 1 + rand()%9;
-    }
-    for (int i = 0; i < 4; i++){
-        cout << array1[i] << " ";
-    }
-    cout << endl;
-    cout << "Second array: ";
-    for (int i = 0; i < 4; i++){
         array2 [i] = 1 + rand()%9;
-    }
-    for (int i = 0; i < 4; i++){
-        cout << array2[i] << " ";
     }
     for (int i = 0,buffer = 1000; i<4;i++){
         summ1 += array1[i]*buffer;
+        summ2 += array2[i]*buffer;
         buffer = buffer/10;
     }
-    for (int j = 0,buffer = 1000; j < 4;j++){
-        summ2 += array2[j]*buffer;
-        buffer = buffer/10;
-    }
+    cout << "First array: " << summ1 << endl;
+    cout << "Second array: " << summ2;
     cout << endl;
     if (summ1 > summ2){
         cout << summ1 << " > " << summ2;
