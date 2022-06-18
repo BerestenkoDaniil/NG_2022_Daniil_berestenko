4a13d6d167d979015fb03403a530354b357902f8
Daniil Berestenko <berestenkodaniilki212@gmail.com>
2022-06-18 19:26:28 +0300

    Modified task_4 with condition

--- Lection_4/task_4/main.cpp
+++ Lection_4/task_4/main.cpp
@@ -10,7 +10,13 @@ int main()
     cin.get(YourString, 255);
     for (int index = 0; YourString[index+1] != '\0'; index++)
     {
-        if((((YourString[index] >= 'a' &&YourString[index] <= 'z') || (YourString[index] >= 'A' && YourString[index] <= 'Z')) && !((YourString[index+1] >= 'a' && YourString[index+1] <= 'z') || (YourString[index+1] >= 'A' && YourString[index+1] <= 'Z'))) || (YourString[index] >= 'a' &&YourString[index] <= 'z') && (YourString[index+1] >= 'A' &&YourString[index+1] <= 'Z'))
+        if((((YourString[index] >= 'a' &&YourString[index] <= 'z') ||
+             (YourString[index] >= 'A' && YourString[index] <= 'Z')) &&
+            !((YourString[index+1] >= 'a' && YourString[index+1] <= 'z') ||
+              (YourString[index+1] >= 'A' && YourString[index+1] <= 'Z'))) ||
+           (YourString[index] >= 'a' &&YourString[index] <= 'z') &&
+           (YourString[index+1] >= 'A' &&YourString[index+1] <= 'Z'))
+
             amount++;
     }
     cout << "Result: " << amount;
